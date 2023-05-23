c2bfd6b012f9ee2c99320fb09ca12c278de7ef02
Yewon Kim <yewon.devv@gmail.com>
2023-05-23 19:41:58 +0900

    feat: backend 환경 세팅 (#4)

---
+++ backend/crewz-admin/src/main/java/crewz/admin/crewzadmin/CrewzAdminApplication.java
@@ -0,0 +1,13 @@
+package crewz.admin.crewzadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewzAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewzAdminApplication.class, args);
+	}
+
+}
