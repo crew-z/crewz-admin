0f91e43cb511123d457bfee87e0ded69f99636cb
Yewon-dev <yewon.devv@gmail.com>
2023-05-23 20:10:08 +0900

    feat: backend, frontend 연동

--- crewz-admin-front/vue.config.js
+++ crewz-admin-front/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  outputDir: "../backend/crewz-admin/src/main/resources/static",
+
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8082",
+        changeOrigin: true,
+      }
+    }
+  }
 });
