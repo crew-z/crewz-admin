b885d4ba1f98143cf82fdc83cc82cb9095464c9a
LEEGWONSEOK <leestart1023@gmail.com>
2023-05-30 16:48:25 +0900

    fix: 새로고침 에러 해결

--- crewz-admin-front/vue.config.js
+++ crewz-admin-front/vue.config.js
@@ -4,14 +4,15 @@ module.exports = defineConfig({
   runtimeCompiler: true,
   lintOnSave: false,
 
-  outputDir: "../backend/crewz-admin/src/main/resources/static",
+  // outputDir: "../backend/crewz-admin/src/main/resources/static",
 
-  devServer: {
-    proxy: {
-      "/": {
-        target: "http://localhost:8082",
-        changeOrigin: true,
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     "/": {
+  //       target: "http://localhost:8082",
+  //       changeOrigin: true,
+  //       ws: false,
+  //     },
+  //   },
+  // },
 });
