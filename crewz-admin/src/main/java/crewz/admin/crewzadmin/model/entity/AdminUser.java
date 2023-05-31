5933fb8d01a106a45ab6e4b991824baaea74184f
Yewon-dev <yewon.devv@gmail.com>
2023-05-31 16:55:47 +0900

    feat: spring-security 적용
    - WebSecurityConfig 파일에서 관리자 로그인과 관련된 필터 적용

---
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/model/entity/AdminUser.java
@@ -0,0 +1,122 @@
+package crewz.admin.crewzadmin.model.entity;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class AdminUser implements UserDetails {
+	@Id
+	private Long adminNo;
+	private String adminId;
+	private String adminName;
+	private String adminPassword;
+	private String adminTel;
+	private String adminEmail;
+	private String adminRoles;
+
+	@Builder
+	public AdminUser(Long adminNo, String adminId, String adminName, String adminPassword,
+		String adminTel, String adminEmail, String adminRoles) {
+		this.adminNo = adminNo;
+		this.adminName = adminName;
+		this.adminId = adminId;
+		this.adminPassword = adminPassword;
+		this.adminTel = adminTel;
+		this.adminEmail = adminEmail;
+		this.adminRoles = adminRoles;
+	}
+
+	/**
+	 * Returns the authorities granted to the user. Cannot return <code>null</code>.
+	 *
+	 * @return the authorities, sorted by natural key (never <code>null</code>)
+	 */
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	}
+
+	/**
+	 * Returns the password used to authenticate the user.
+	 *
+	 * @return the password
+	 */
+	@Override
+	public String getPassword() {
+		return adminPassword;
+	}
+
+	/**
+	 * Returns the username used to authenticate the user. Cannot return
+	 * <code>null</code>.
+	 *
+	 * @return the username (never <code>null</code>)
+	 */
+	@Override
+	public String getUsername() {
+		return adminName;
+	}
+
+	/**
+	 * Indicates whether the user's account has expired. An expired account cannot be
+	 * authenticated.
+	 *
+	 * @return <code>true</code> if the user's account is valid (ie non-expired),
+	 * <code>false</code> if no longer valid (ie expired)
+	 */
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	/**
+	 * Indicates whether the user is locked or unlocked. A locked user cannot be
+	 * authenticated.
+	 *
+	 * @return <code>true</code> if the user is not locked, <code>false</code> otherwise
+	 */
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	/**
+	 * Indicates whether the user's credentials (password) has expired. Expired
+	 * credentials prevent authentication.
+	 *
+	 * @return <code>true</code> if the user's credentials are valid (ie non-expired),
+	 * <code>false</code> if no longer valid (ie expired)
+	 */
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	/**
+	 * Indicates whether the user is enabled or disabled. A disabled user cannot be
+	 * authenticated.
+	 *
+	 * @return <code>true</code> if the user is enabled, <code>false</code> otherwise
+	 */
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
---
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package crewz.admin.crewzadmin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import crewz.admin.crewzadmin.model.entity.AdminUser;
+
+public interface AdminRepository extends JpaRepository<AdminUser, Long> {
+	AdminUser findByAdminId(String userId);
+}
