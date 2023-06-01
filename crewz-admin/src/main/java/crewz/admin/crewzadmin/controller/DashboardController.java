c6fa01eef060bdebecb918c8ef7ae10387427a36
LEEGWONSEOK <leestart1023@gmail.com>
2023-06-01 14:15:41 +0900

    feat: 대시보드 DTO, Service, Controller 생성 및 수정
    - AdminUser 관련은 주석으로 처리

--- crewz-admin/src/main/java/crewz/admin/crewzadmin/controller/DashboardController.java
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/controller/DashboardController.java
@@ -1,8 +1,12 @@
 package crewz.admin.crewzadmin.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import crewz.admin.crewzadmin.model.dto.ResponseDashboardDto;
+import crewz.admin.crewzadmin.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,5 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class DashboardController {
+	private final DashboardService dashboardService;
 
+	@GetMapping("/main")
+	public ResponseEntity<ResponseDashboardDto> getDashboard() {
+		ResponseDashboardDto res = dashboardService.getDashboard();
+		return ResponseEntity.ok(res);
+	}
 }
--- crewz-admin/src/main/java/crewz/admin/crewzadmin/service/DashboardService.java
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/service/DashboardService.java
@@ -1,4 +1,32 @@
 package crewz.admin.crewzadmin.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import crewz.admin.crewzadmin.model.dto.ResponseDashboardDto;
+import crewz.admin.crewzadmin.repository.BoardCategoryRepository;
+import crewz.admin.crewzadmin.repository.ClubRepository;
+// import crewz.admin.crewzadmin.repository.SubsidyRepository;
+import crewz.admin.crewzadmin.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class DashboardService {
+	private final UserRepository userRepository;
+	private final ClubRepository clubRepository;
+	private final BoardCategoryRepository boardCategoryRepository;
+	// private final SubsidyRepository subsidyRepository;
+
+
+	public ResponseDashboardDto getDashboard() {
+		long totalUserCount = userRepository.countBy();
+		long totalClubCount = clubRepository.count();
+		long totalOperatingClubs = clubRepository.countByClubCloseYn("N");
+		List<Object[]> clubCountByCategory = boardCategoryRepository.countByCategory();
+		// long totalSubsidyPrice = subsidyRepository.sumPrice();
+
+		return new ResponseDashboardDto(totalUserCount, totalClubCount, totalOperatingClubs, clubCountByCategory);
+	}
 }
