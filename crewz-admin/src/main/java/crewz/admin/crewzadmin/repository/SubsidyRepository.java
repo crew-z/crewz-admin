7adac0cd4511adce5dcb79c994d92228022b0ec0
LEEGWONSEOK <leestart1023@gmail.com>
2023-06-01 14:14:39 +0900

    feat: 엔티티, 레포지토리 생성 및 수정
    - Board, BoardCategory, BoardPeriod, BoardPeriodId, CategoryInfo, Club, ClubApply, ClubInfo, Subsidy, User 엔티티 생성 및 수정
    - 동일하게 레포도 수정

--- crewz-admin/src/main/java/crewz/admin/crewzadmin/repository/SubsidyRepository.java
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/repository/SubsidyRepository.java
@@ -1,9 +1,11 @@
-package crewz.admin.crewzadmin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import crewz.admin.crewzadmin.model.entity.Subsidy;
-
-public interface SubsidyRepository extends JpaRepository<Subsidy, Long> {
-
-}
+// package crewz.admin.crewzadmin.repository;
+//
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+//
+// import crewz.admin.crewzadmin.model.entity.Subsidy;
+//
+// public interface SubsidyRepository extends JpaRepository<Subsidy, Long> {
+// 	@Query("SELECT SUM(s.price) FROM Subsidy s")
+// 	Long sumPrice();
+// }
