10d698a1d2c92e62e3509ef20265a334217bb553
Yewon-dev <yewon.devv@gmail.com>
2023-06-02 10:02:29 +0900

    feat: Admin 리스트 생성
    - Admin 관리를 위한 리스트 생성
    - 관리자 등록 버튼 생성
    - 관리자 등록을 위한 form 생성
    - 관리자 등록 모달창 생성
    
    related to #30

--- crewz-admin-front/src/router/index.js
+++ crewz-admin-front/src/router/index.js
@@ -3,7 +3,11 @@ import { createRouter, createWebHistory } from "vue-router";
 // Default Pages
 import Dashboard from "../views/AdminDashboard.vue";
 // Clubmanagement pages
-import ClubInfo from "../views/components/AdminClubInfo.vue";
+// import ClubInfo from "../views/components/AdminClubInfo.vue";
+
+// Manage Admin Pages
+import AdminManage from "../views/components/ManageAdmin.vue";
+import WithdrawalUser from "../views/components/WithdrawalUser.vue";
 
 // Component Pages
 import Valert from "../views/components/AdminAlert.vue";
@@ -32,23 +36,23 @@ import Tables from "../views/AdminTables.vue";
 var appname = " - Windzo Dashboard Admin Template";
 
 const routes = [
-	// Routes
-	{
-		path: "/",
-		name: "Dashboard",
-		component: Dashboard,
-		meta: { title: "Dashboard " + appname },
-	},
+  // Routes
+  {
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: { title: "Dashboard " + appname },
+  },
 
-	// Clubmanagement based Routes
-	{
-		path: "/clubmenagement/clubdashboard",
-		name: "ClubInfo",
-		component: ClubInfo,
-		meta: { title: "Clubinfo" + appname },
-	},
+  // Clubmanagement based Routes
+  //   {
+  //     path: "/clubmenagement/clubdashboard",
+  //     name: "ClubInfo",
+  //     component: ClubInfo,
+  //     meta: { title: "Clubinfo" + appname },
+  //   },
 
-	// Components based Routes
+  // Components based Routes
   {
     path: "/category",
     name: "category",
@@ -60,6 +64,18 @@ const routes = [
     name: "clubtable",
     component: VclubTable,
     meta: { title: "clubtable" + appname },
+  },
+  {
+    path: "/admin",
+    name: "adminManage",
+    component: AdminManage,
+    meta: { title: "admin" + appname },
+  },
+  {
+    path: "/admin/withdrawal",
+    name: "withdrawal",
+    component: WithdrawalUser,
+    meta: { title: "withdrawal" + appname },
   },
   {
     path: "/component/alert",
@@ -117,67 +133,67 @@ const routes = [
     component: Blank,
     meta: { title: "Blank Page" + appname },
   },
-	{
-		path: "/auth/login",
-		name: "Login",
-		component: Login,
-		meta: { title: "Login" + appname, hideNav: true },
-	},
-	{
-		path: "/auth/register",
-		name: "Register",
-		component: Register,
-		meta: { title: "Register" + appname, hideNav: true },
-	},
-	{
-		path: "/auth/forgot-password",
-		name: "ForgotPassword",
-		component: ForgotPassword,
-		meta: { title: "i Forgot Password" + appname, hideNav: true },
-	},
-	// layout/error
-	// default page 404
-	{
-		path: "/:pathMatch(.*)*",
-		name: "Page404",
-		component: Page404,
-		meta: { title: "Upps! 404" + appname, hideNav: true },
-	},
-	{
-		path: "/500",
-		name: "Page500",
-		component: Page500,
-		meta: { title: "Server internal Error" + appname, hideNav: true },
-	},
-	{
-		path: "/maintenance",
-		name: "maintenance",
-		component: PageMaintenance,
-		meta: {
-			title: "Sorry the app has been Maintenance" + appname,
-			hideNav: true,
-		},
-	},
-	{
-		path: "/tables",
-		name: "Tables",
-		component: Tables,
-		meta: {
-			title: "Tables" + appname,
-		},
-	},
+  {
+    path: "/auth/login",
+    name: "Login",
+    component: Login,
+    meta: { title: "Login" + appname, hideNav: true },
+  },
+  {
+    path: "/auth/register",
+    name: "Register",
+    component: Register,
+    meta: { title: "Register" + appname, hideNav: true },
+  },
+  {
+    path: "/auth/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta: { title: "i Forgot Password" + appname, hideNav: true },
+  },
+  // layout/error
+  // default page 404
+  {
+    path: "/:pathMatch(.*)*",
+    name: "Page404",
+    component: Page404,
+    meta: { title: "Upps! 404" + appname, hideNav: true },
+  },
+  {
+    path: "/500",
+    name: "Page500",
+    component: Page500,
+    meta: { title: "Server internal Error" + appname, hideNav: true },
+  },
+  {
+    path: "/maintenance",
+    name: "maintenance",
+    component: PageMaintenance,
+    meta: {
+      title: "Sorry the app has been Maintenance" + appname,
+      hideNav: true,
+    },
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    component: Tables,
+    meta: {
+      title: "Tables" + appname,
+    },
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 
-	linkExactActiveClass: "exact-active",
+  linkExactActiveClass: "exact-active",
 });
 
 router.beforeEach((to, from, next) => {
-	document.title = to.meta.title;
-	next();
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
