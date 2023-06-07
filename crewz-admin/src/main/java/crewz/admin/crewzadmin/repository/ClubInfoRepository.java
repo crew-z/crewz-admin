e6a17313846068a23fb5cd01a73a9276b88c9b02
Yewon-dev <yewon.devv@gmail.com>
2023-06-07 16:00:16 +0900

    feat: 사용자 탈퇴 기능
    - 사용자 탈퇴 클릭 시, 사용자가 가입한 동아리 정보에서 사용자 정보 제거

--- crewz-admin/src/main/java/crewz/admin/crewzadmin/repository/ClubInfoRepository.java
+++ crewz-admin/src/main/java/crewz/admin/crewzadmin/repository/ClubInfoRepository.java
@@ -1,8 +1,13 @@
 package crewz.admin.crewzadmin.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import crewz.admin.crewzadmin.model.entity.ClubInfo;
 
 public interface ClubInfoRepository extends JpaRepository<ClubInfo, Long> {
+	List<ClubInfo> findAllByUser_UserNo(Long userNo);
 }
