62d31303cc63bdf907b5e71b967227cabbc03d1d
Yewon-dev <yewon.devv@gmail.com>
2023-06-08 16:43:02 +0900

    feat: 로그아웃 기능 구현
    - 로그아웃시, 쿠키 삭제 후 로그인 페이지로 이동
    - 로그인 시, 사이드바에 회원 이름 노출
    
    resolves: #66

--- crewz-admin-front/src/components/AdminSidebar.vue
+++ crewz-admin-front/src/components/AdminSidebar.vue
@@ -8,19 +8,16 @@
           class="w-8 mt-2"
           src="@/assets/logo/logo-admin.svg"
           alt="crewz logo"
-          style="width: 160px; height: 40px"
-        />
+          style="width: 160px; height: 40px" />
       </router-link>
     </div>
     <div class="sidebar-head pt-4 px-4 pb-2">
       <p>
-        <span class="mr-4 text-lg">adminUserName{{ adminUserName }}</span>
+        <span class="mr-4 text-xl font-semibold">{{ adminUserName }}</span>
         <button
           type="button"
-          class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
-          style="background-color: #ff5a5a"
-          @click="logout"
-        >
+          class="text-white bg-[#ff5a5a] hover:bg-[#ff1a1a] focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+          @click="logout">
           로그아웃
         </button>
       </p>
@@ -34,8 +31,7 @@
             <router-link
               to="/"
               exact
-              class="w-full flex text-left rounded-md box-border p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-            >
+              class="w-full flex text-left rounded-md box-border p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
               <span class="mr-3 text-xl"><Icon icon="bxs:dashboard" /></span>
               <span class="w-full"> 대시보드 </span>
             </router-link>
@@ -49,20 +45,17 @@
               <template v-slot:content>
                 <router-link
                   to="/category"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   카테고리 관리
                 </router-link>
                 <router-link
                   to="/clubtable"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   동아리 목록
                 </router-link>
                 <router-link
                   to="/clubapplytable"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   동아리 신청 목록
                 </router-link>
               </template>
@@ -88,14 +81,12 @@
               <template v-slot:content>
                 <router-link
                   to="/admin"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   관리자 목록
                 </router-link>
                 <router-link
                   to="/admin/withdrawal"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   회원 탈퇴
                 </router-link>
               </template>
@@ -114,8 +105,7 @@
               <div class="">
                 <router-link
                   to="/blank"
-                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                >
+                  class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                   Blank Page
                 </router-link>
                 <menu-accordion>
@@ -123,20 +113,17 @@
                   <template v-slot:content>
                     <router-link
                       to="/404page"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       404 page
                     </router-link>
                     <router-link
                       to="/500"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       500 page
                     </router-link>
                     <router-link
                       to="/maintenance"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       Maintenance page
                     </router-link>
                   </template>
@@ -146,20 +133,17 @@
                   <template v-slot:content>
                     <router-link
                       to="/auth/login"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       Login
                     </router-link>
                     <router-link
                       to="/auth/register"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       Register
                     </router-link>
                     <router-link
                       to="/auth/forgot-password"
-                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
+                      class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                       Forgot Password
                     </router-link>
                   </template>
@@ -167,50 +151,42 @@
               </div>
               <router-link
                 to="/component/alert"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Alert
               </router-link>
               <router-link
                 to="/component/accordion"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Accordion
               </router-link>
               <router-link
                 to="/component/badge"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Badge
               </router-link>
               <router-link
                 to="/component/breadcumb"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Breadcumb
               </router-link>
               <router-link
                 to="/component/button"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Button
               </router-link>
               <router-link
                 to="/component/card"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Card
               </router-link>
               <router-link
                 to="/component/dropdown"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Dropdown
               </router-link>
               <router-link
                 to="/component/modal"
-                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700"
-              >
+                class="w-full text-left block rounded-md p-3 hover:bg-gray-200 dark:hover:bg-gray-700">
                 Modal
               </router-link>
             </template>
@@ -222,26 +198,26 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { getAdminUserName } from "@/api/sidebar";
 import { Icon } from "@iconify/vue";
 import MenuAccordion from "./MenuAccordion.vue";
+import { useCookies } from "vue3-cookies";
+import store from "@/store/index";
 
-const adminUserName = ref("");
+const cookies = useCookies().cookies;
+
+const adminUserName = store.state.name;
 
 const logout = () => {
-  alert("로그아웃을 진행하나요?");
+  let logout = confirm("로그아웃 하시겠습니까?");
+  if (logout) {
+    cookies.remove("token");
+    cookies.remove("role");
+    window.location.reload();
+  }
   // 로그아웃 처리 로직 넣으면 됨
 };
 
-defineExpose({
-  logout,
-});
-
-const init = async () => {
-  const res = await getAdminUserName();
-  adminUserName.value = ""; // 해당 위치에 res.data.AdminUserName 처럼 넣어보시죵
-};
-
-init();
+// defineExpose({
+//   logout,
+// });
 </script>
